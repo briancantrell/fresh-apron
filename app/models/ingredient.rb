07076efca38ede8a713ff4cbdd786d8033e8df60
Brian Cantrell <bcantrell@gmail.com>
2017-09-09 13:10:33 -0400

    Add teaspoon and tablespoon units

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,14 +1,16 @@
 class Ingredient < ApplicationRecord
-  enum units: [:ounce_volume, :ounce_weight, :item_count]
+  enum units: [:ounce_volume, :ounce_weight, :item_count, :teaspoon, :tablespoon]
 
   validates :name, presence: true
   validates :units, presence: true
 
   def units_abbreviation
     {
-      "ounce_volume" => "oz.",
-      "ounce_weight" => "oz.",
-      "item_count" => "ct."
+      "ounce_volume" => "oz",
+      "ounce_weight" => "oz",
+      "item_count" => "ct",
+      "teaspoon" => "tsp",
+      "tablespoon" => "tbsp"
     }[units]
   end
 
